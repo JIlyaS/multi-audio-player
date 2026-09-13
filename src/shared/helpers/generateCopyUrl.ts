d1fd05f1056a32d05229979ac68af3aefab4356a
Ilia Kolmakov <ilkolmakov@yandex.ru>
2026-09-13 19:11:12 +0500

    feat(main): log

--- src/shared/helpers/generateCopyUrl.ts
+++ src/shared/helpers/generateCopyUrl.ts
@@ -5,12 +5,20 @@ export const generateCopyIncognitoUrl = (
   type: "track" | "playlist",
 ): string => {
   const url = import.meta.env.VITE_APP_EXTERNAL_COPY_URL;
+
+  console.log("generateCopyIncognitoUrl", url);
    
   if (window.location.hostname.includes("localhost")) {
     return `${window.location.protocol}//${window.location.host}${getBasePrefix()}?${type}Id=${id}`;
   }
 
   if (window.location.hostname.includes("multiaudioplayer")) {
+    console.log(
+      "generateCopyIncognitoUrl yes",
+      url,
+      getBasePrefix(),
+      `${window.location.protocol}//${url}${getBasePrefix()}?${type}Id=${id}`,
+    );
     return `${window.location.protocol}//${url}${getBasePrefix()}?${type}Id=${id}`;
   }
 
@@ -22,10 +30,15 @@ export const generateCopyUrl = (
   id: string,
   type: "track" | "playlist",
 ): string => {
+    console.log("generateCopyUrl", window.location.hostname);
     if (
       window.location.hostname.includes("localhost") ||
       window.location.hostname.includes("multiaudioplayer")
     ) {
+        console.log(
+          "generateCopyUrl yes",
+          `${window.location.protocol}//${window.location.host}?${type}Id=${id}`,
+        );
       return `${window.location.protocol}//${window.location.host}?${type}Id=${id}`;
     }
     return `${window.location.protocol}//${window.location.host}/${window.location.pathname}?${type}Id=${id}`;
--- src/shared/helpers/isFilterTrackPlaylistParams.ts
+++ src/shared/helpers/isFilterTrackPlaylistParams.ts
@@ -4,6 +4,8 @@ export const isOnlyParamsTrackData = () => {
   const hostname = window.location.hostname;
   const pathname = window.location.pathname;
 
+  console.log("isOnlyParamsTrackData", hostname, pathname, pathname.replaceAll('/', ''), getBasePrefix());
+
   return (
     (hostname === "localhost" || hostname === "multiaudioplayer") &&
     pathname.replaceAll('/', '') === `${getBasePrefix()}`
@@ -13,6 +15,8 @@ export const isOnlyParamsTrackData = () => {
 
 export const isFilterTrackPlaylistParams = (id: string, trackId: string, playlistId: string): boolean => {
   const isOnlyParamsData = isOnlyParamsTrackData();
+
+  console.log("isFilterTrackPlaylistParams isOnlyParamsData", isOnlyParamsData);
 
   if (!trackId && !playlistId && !isOnlyParamsData) {
     return true;
